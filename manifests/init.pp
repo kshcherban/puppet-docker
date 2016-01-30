71ccfceac6e93cd10114c5a885e99bd63e5a3e45
kshcherban <k.scherban@gmail.com>
2016-01-30 23:05:15 +0300

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,69 @@
+class docker (
+    $version              = 'present',
+# where images and containers are stored
+    $graph_dir            = '/var/lib/docker',
+# custom dns server to use
+    $dns_server           = undef,
+# Daemon socket(s) to connect to, array
+    $host                 = undef,
+# Enable insecure registry communication
+    $insecure_registry    = undef,
+# Name of docker package
+    $package_name         = 'docker-engine',
+# Nginx proxy to expose api port outside
+    $nginx_proxy          = false,
+# Nginx proxy nginx package version
+    $nginx_proxy_ver      = 'present',
+# Nginx basic auth, off by default
+    $nginx_proxy_auth     = false,
+# Nginx allowed ip/subnets array
+    $nginx_proxy_allowip  = undef,
+# Nginx proxy basic auth user
+    $nginx_proxy_user     = 'docker',
+# Nginx proxy basic auth password
+    $nginx_proxy_pass     = 'docker',
+# Nginx proxy port
+    $nginx_proxy_port     = 4243,
+) {
+    package {$package_name:
+        ensure  => $version,
+    }
+    ->
+    service {'docker':
+        ensure => 'running',
+    }
+    file {'/etc/default/docker':
+        ensure  => 'present',
+        content => template('docker/default.docker.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        notify  => Service['docker'],
+        require => Package[$package_name],
+    }
+    if $nginx_proxy {
+        package {'nginx':
+            ensure  => $nginx_proxy_ver,
+            require => Service['docker'],
+        }
+        ->
+# Changing nginx group needed to access socket
+        user {'nginx':
+            groups => ['docker'],
+        }
+        ->
+        file {'/etc/nginx/.passwd':
+            ensure  => 'present',
+            content => template('docker/nginx.passwd.erb'),
+        }
+        ->
+        file {'/etc/nginx/sites-enabled/docker.conf':
+            ensure  => 'present',
+            content => template('docker/nginx-proxy-docker.conf.erb'),
+        }
+        ~>
+        service {'nginx':
+            ensure => 'running',
+        }
+    }
+}
